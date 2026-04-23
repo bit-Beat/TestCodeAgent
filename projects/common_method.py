d1075ae83ac2768cbe8bf61196383887b31b57f3
bit-Beat <ehwns4337@naver.com>
2026-04-23 11:28:51 +0900

    기본구조 생성

---
+++ projects/common_method.py
@@ -0,0 +1,72 @@
+
+""" 공통 함수 """
+
+def log(message: str, level: str = "PRINT") -> None:
+    """사용자 입력 로그 출력 함수"""
+    level = level.upper()
+
+    prefix_map = {
+        "INFO": "\n[ℹ️INFO]",
+        "WARNING": "\n[⚠️WARNING]",
+        "ERROR": "\n[❌ERROR]",
+        "DEBUG": "\n[🐛DEBUG]",
+        "PRINT": "→"
+    }
+
+    prefix = prefix_map.get(level, "")
+
+    print(f"{prefix} {message}")
+    if level != "PRINT":
+        print("─" * 20)  
+
+def pretty_trace(message):
+    """DeepAgents 실행 결과(messages)를 사람이 읽기 쉬운 형태로 정리하여 출력하는 함수."""
+    step = 1
+
+    if not isinstance(message, dict):
+        WARNING_MESSAGE("Not Dict Type!")
+        return 
+
+    if "messages" not in message.keys():
+        WARNING_MESSAGE("DeepAgent 메시지 결과 값 전달 바랍니다.")
+        return
+
+    messages = message['messages']
+    
+    for msg in messages:
+        msg_type = msg.__class__.__name__
+
+        if msg_type == "HumanMessage":
+            log(f"[STEP {step}]🧑 USER INPUT", "info")
+            log(f" {msg.content}")
+            
+        elif msg_type == "AIMessage":
+            if msg.tool_calls:
+                log(f"[STEP {step}]🤖 AI AGENT", "info")
+
+                for tc in msg.tool_calls:
+                    tool_name = tc.get("name")
+                    args = tc.get("args", {})
+
+                    if tool_name == "task":
+                        subagent = args.get("subagent_type", "UNKNOWN")
+                        desc = args.get("description", "")
+                        log(f" SubAgent 호출: {subagent}")
+                        log(f" Args: {desc}")
+                    else:
+                        log(f" Tool: {tool_name}")
+                        log(f" Args: {args}")
+            else:
+                log(f"[STEP {step}]🚀 FINAL RESPONSE", "info")
+                log(f" {msg.content}")
+                
+
+        elif msg_type == "ToolMessage":
+            if msg.name == "task":
+                log(f"[STEP {step}]🛠 SUBAGENT RESULT", "info")
+                log(f" Result: {msg.content}")
+            else:
+                log(f"[STEP {step}]🛠 TOOL RESULT", "info")
+                log(f" Toolname : {msg}")
+                log(f" Result: {msg.content}")
+        step += 1
---
+++ projects/main.py
@@ -0,0 +1,12 @@
+#from agents.orchestrator import orchestrator
+from common_method import log, pretty_trace
+
+#code_input = """
+#public void createNewEngtFoup(Document doc) {
+#    // new logic
+#}
+#"""
+
+
+
+log("hello")
